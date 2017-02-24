1bd3414865c6e89760edf385431825d508f61381
Brandon <brandon.ayi@telecomnancy.net>
2017-02-24 16:50:30 +0100

    correct tense mis à jour

--- MyEnglishApp/app/src/main/java/com/example/brandon/myenglishapp/CorrectTenseActivity.java
+++ MyEnglishApp/app/src/main/java/com/example/brandon/myenglishapp/CorrectTenseActivity.java
@@ -7,7 +7,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,46 +28,66 @@ public class CorrectTenseActivity extends Activity implements OnItemSelectedList
         Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
         Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
         Spinner spinner5 = (Spinner) findViewById(R.id.spinner5);
-        Spinner spinner6 = (Spinner) findViewById(R.id.spinner6);
 
         // Spinner click listener
         spinner1.setOnItemSelectedListener(this);
-
         // Spinner Drop down elements
         List<String> categories1 = new ArrayList<String>();
         categories1.add("has been appearing");
         categories1.add("has appeared");
         categories1.add("appeared");
-
         // Creating adapter for spinner
         ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories1);
-
         // Drop down layout style - list view with radio button
         dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(dataAdapter1);
 
 
         spinner2.setOnItemSelectedListener(this);
-
         List<String> categories2 = new ArrayList<String>();
-        categories2.add("has been appearing");
-        categories2.add("has appeared");
-        categories2.add("appeared");
-
+        categories2.add("spilt");
+        categories2.add("have been spilling");
+        categories2.add("have spilt");
         ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories2);
-
         dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
         spinner2.setAdapter(dataAdapter2);
+
+
+        spinner3.setOnItemSelectedListener(this);
+        List<String> categories3 = new ArrayList<String>();
+        categories3.add("tried");
+        categories3.add("have tried");
+        categories3.add("have been trying");
+        ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories3);
+        dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(dataAdapter3);
+
+        spinner4.setOnItemSelectedListener(this);
+        List<String> categories4 = new ArrayList<String>();
+        categories4.add("have applied");
+        categories4.add("have been applying");
+        categories4.add("applied");
+        ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories4);
+        dataAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(dataAdapter4);
+
+        spinner5.setOnItemSelectedListener(this);
+        List<String> categories5 = new ArrayList<String>();
+        categories5.add("have been boiling");
+        categories5.add("have boiled");
+        categories5.add("boiled");
+        ArrayAdapter<String> dataAdapter5 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories5);
+        dataAdapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner5.setAdapter(dataAdapter5);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
+        //String item = parent.getItemAtPosition(position).toString();
 
         // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+        //Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
